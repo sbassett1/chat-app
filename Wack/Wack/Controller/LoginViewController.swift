81c28f8214cf153414ef15c62671313e8c1de1e9
Stephen Bassett <sbassett1@gmail.com>
2019-05-14 11:55:02 -0500

    created login VC and UI layout

---
+++ Wack/Wack/Controller/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Wack
+//
+//  Created by Stephen Bassett on 5/14/19.
+//  Copyright © 2019 Stephen Bassett. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: App Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: Actions
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Wack/Wack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Wack
+//
+//  Created by Stephen Bassett on 5/14/19.
+//  Copyright © 2019 Stephen Bassett. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: Segues
+
+let TO_LOGIN = "toLogin"
